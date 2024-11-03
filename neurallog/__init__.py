5cd694f7676a3211a2f9e21a3c465d473e98fd1e
Dinh Doan Xuan Phuong <115974903+phuong0111@users.noreply.github.com>
2024-11-03 16:29:21 +0700

    migrate to torch 04

--- neurallog/__init__.py
+++ neurallog/__init__.py
@@ -1,3 +1,6 @@
-from models import *
+from models.transformers import *
+from models.positional_encodings import *
+from models.log2vec import *
+from models.logrobust import *
 from .utils import *
 from .data_loader import *
